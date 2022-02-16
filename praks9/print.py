8f54166ee0a91c6510c2c168eb050cd5ae608a4b
Kristjan <ucloud16085@gmail.com>
2022-02-16 14:56:09 +0200

    dokumenteerimine

--- praks9/print.py
+++ praks9/print.py
@@ -1,5 +1,6 @@
 from praks9.koodi_dokumenteerimine import AknadUksed, Tuba
 
+# Küsib ruumi/toa mõõtusid
 print("Ruumi mõõdud: ")
 p = float(input("Pikkus: "))
 l = float(input("Laius: "))
@@ -7,7 +8,10 @@ k = float(input("Kõrgus: "))
 
 tuba = Tuba(p, l, k)
 
+# Küsib kas on olemas aknad või uksed
 vastus = input("Kas olemas on aknad/usked? (jah/ei) ")
+
+# Kui "jah", siis küsib akna või ukse laiust ja kõrgust
 while vastus == "jah":
     l = float(input("Akna/Ukse laius "))
     k = float(input("Akna/Ukse kõrgus "))
@@ -15,9 +19,13 @@ while vastus == "jah":
     tuba.aknad_uksed.append(aken_uks)
     vastus = input("Kas olemas aknad/uksed? (jah/ei) ")
 
+# Küsib tapeedi rulli mõõte
 print("Tapeedi rulli mõõtud: ")
 tl = float(input("Tapeedi rulli laius "))
 tp = float(input("Tapeedi rulli pikkus "))
 
+# Väljastab mitu ruutmeetrit tapeeti on vaja kleepida seinale
 print("Tapeedid on vaja kleepida " + str(tuba.tooPind()) + " ruutmeetrile")
+
+# Väljastab mitu tapeedi rulli läheb vaja
 print("Tapeedi rullida arv " + str(tuba.tapeedid(tp, tl)))
